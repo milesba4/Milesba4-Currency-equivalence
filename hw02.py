79f1172e80c7f35241ddf41acc8a18716689678d
milesba4 <milesbaffour@gmail.com>
2021-10-24 19:15:05 -0700

    Update hw02.py

--- hw02.py
+++ hw02.py
@@ -5,11 +5,20 @@ with open(file, encoding = 'ascii') as k:
     text = k.read()
     currency_str= json.loads(text)
 
+
+count = {'JPY': 114, 'ALL': 104.69, 'ARS': 99.38, 'DZD': 137.19}
+
 names = ['JPY', 'ALL', 'ARS','DZD']
 values = [currency_str["rates"]['JPY'], currency_str["rates"]['ALL'], currency_str["rates"]['ARS'], currency_str["rates"]['DZD']]
 
+
+
+
+}
 plt.figure(figsize=(9, 3))
 
 plt.subplot(131)
 plt.bar(names, values)
 plt.subplot(132)
+
+plt.show()
